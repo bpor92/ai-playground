a9b3ed733b468e941adb01cf230d0508f41bd82d
Bartek <bartoszporebski199@gmail.com>
2023-04-29 21:16:29 +0200

    add navbar

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <NuxtPage  />
+  <Navbar/>
+  <div class="md:container md:mx-auto">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+  <div class="max-w-full w-[900px] m-auto p-5">
+    <ChatBox/>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,10 @@
 <template>
   <div>
-    <UiSwap v-model="swap">
-      <template v-slot:on>
-        <IconsMoon />
-      </template>
-      <template v-slot:off>
-        <IconsSun />
-      </template>
-    </UiSwap>
-
-    <div class="max-w-full w-[900px] m-auto p-5">
-      <ChatBox/>
-    </div>
+   dasdasdasdas
   </div>
 </template>
 
 <script setup lang="ts">
-import { Theme } from '~~/types/theme'
-const swap = ref(true)
-
-watch(swap , (value: Boolean) => {
-  const theme = value ? Theme.DARK : Theme.CUPCAKE 
-  document.querySelector('html')!.setAttribute('data-theme', theme);
-})
 
 </script>
 
