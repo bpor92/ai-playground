10c7320cf1173ca34004bff25f74eb818fa90fc1
Bartek <bartoszporebski199@gmail.com>
2023-05-05 18:26:27 +0200

    add navigation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,11 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
+          <li>
+            <NuxtLink to="/ui">
+              UI
+            </NuxtLink>
+          </li>
           <li tabindex="0">
             <a class="justify-between">
               Parent
@@ -20,7 +24,11 @@
           <li><a>Item 3</a></li>
         </ul>
       </div>
-      <a class="btn btn-ghost normal-case text-xl">Training</a>
+      <a class="btn btn-ghost normal-case text-xl">
+        <NuxtLink to="/">
+          Training
+        </NuxtLink> 
+      </a>
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
