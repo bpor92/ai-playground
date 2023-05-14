ed6a52d5843c97febb186b082d40a94f96f93857
Bartek <bartoszporebski199@gmail.com>
2023-05-14 17:24:37 +0200

    add response ddescription loader

--- pages/job-description-generator.vue
+++ pages/job-description-generator.vue
@@ -6,24 +6,25 @@
   <br>
 
   <UiForm>
-    <div>
+    <div >
       <UiElSelect 
         :options="jobs"
         filterable
-        label="Job position"
+        label="Employee position"
         v-model="form.position"
       />
       <UiButton v-if="form.position" mode="link" @click="prepareTasks">Prepare Tasks</UiButton>
     </div>
 
     <UiElTextarea v-model="form.tasks" label="Tasks" :loading="prepareTasksLoader" />
+    
     <br>
-    <UiButton mode="primary" @click="generateJobDescription">Generate</UiButton>
+    <UiButton  mode="primary" @click="generateJobDescription">Generate</UiButton>
   </UiForm>
-
+  
   <br>
-
-  <UiMockupWindow  >
+  
+  <UiMockupWindow v-show="responseDescription || responseDescriptionLoader" v-loading="responseDescriptionLoader"  >
     <Markdown :source="responseDescription" class="p-5 break-words" />
   </UiMockupWindow>
 
@@ -42,18 +43,21 @@ const form = reactive({
 })
 
 const responseDescription = ref('')
+const responseDescriptionLoader  = ref(false)
 const generateJobDescription = async () => {
   const { api } = useJobDescriptionGnerator()
 
+
   const data = {
-    position: form.position,
+    position: useArrayFind(jobs, val => val.value === form.position).value?.label,
     tasks: form.tasks
   }
-
+  responseDescriptionLoader.value = true
   const response = await api(data)
   if (!response.choices[0].message?.content) return
 
   responseDescription.value = response.choices[0].message?.content
+  responseDescriptionLoader.value = false
 }
 
 const prepareTasksLoader = ref<boolean>(false)
