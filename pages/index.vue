59c53c632d5fb0aa72dfa3603ebd8e3f4716af9f
Bartek <bartoszporebski199@gmail.com>
2023-05-23 10:22:41 +0200

    delete i18n

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
-    <UiForm  class="w-full  p-2 md:max-w-sm md:mx-auto">
-      <UiElSelect 
-        v-model="locale"
-        label="'lang'"
-        :options="[{ label: 'pl', value: 'pl'}, {value: 'en', label: 'en'}]"
-      />
-    </UiForm>
+    CUMULUS TEAM
   </div>
 </template>
 
-<script setup>
-const { locale } = useI18n()
-</script>
 
