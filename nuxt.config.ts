c403b2f4b0b1e3f1c2e7d10ae68f07965eead30f
Bartek <bartoszporebski199@gmail.com>
2023-05-26 19:14:52 +0200

    add nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", '@element-plus/nuxt'],
-    ssr: false,
-    runtimeConfig: {
-        OPENAI_API_KEY: "",
-        AZURE_API_KEY: "",
-        public: {
-            MOCK_DATA: true
-        }
-    },
-   
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", '@element-plus/nuxt'],
+  ssr: false,
+
+  runtimeConfig: {
+      OPENAI_API_KEY: "",
+      AZURE_API_KEY: "",
+      public: {
+          MOCK_DATA: true
+      }
+  },
+
+  devtools: {
+    enabled: true,
+  },
 })
